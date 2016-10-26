b8d47091e0af5eff954198d68d804cb511ad5dc5
Ashley Hill <me@ashleyhill.me>
2016-10-26 16:36:49 -0400

    Basic setup #1
    Basic setup of Angular 2 build using webpack. from [https://angular.io/docs/ts/latest/guide/webpack.html]

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import './../styles/styles.css';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent { }
